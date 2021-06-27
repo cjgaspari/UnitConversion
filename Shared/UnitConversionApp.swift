b090b42e18fed708a6a81da10c6a332d68baf72a
CJ Gaspari <cjgaspari@users.noreply.github.com>
2021-06-27 16:45:13 -0700

    Initial Commit

---
+++ Shared/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  Shared
+//
+//  Created by CJ Gaspari on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
